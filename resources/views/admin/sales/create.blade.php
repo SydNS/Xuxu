e8b79b469b89951c5dfed6f3d5e42192867ab497
SydNS <sydneynsiimbe@gmail.com>
2024-06-05 12:43:18 +0300

    corrects sales form

--- resources/views/admin/sales/create.blade.php
+++ resources/views/admin/sales/create.blade.php
@@ -1,71 +1,73 @@
 @extends('layouts.admin')
 @section('content')
+    <div class="card">
+        <div class="card-header">
+            {{ trans('global.create') }} {{ trans('cruds.sale.title_singular') }}
+        </div>
 
-<div class="card">
-    <div class="card-header">
-        {{ trans('global.create') }} {{ trans('cruds.sale.title_singular') }}
+        <div class="card-body">
+            <form method="POST" action="{{ route('admin.sales.store') }}" enctype="multipart/form-data">
+                @csrf
+                <div class="form-group">
+                    <label class="required" for="product_id">{{ trans('cruds.sale.fields.product') }}</label>
+                    <select class="form-control select2 {{ $errors->has('product') ? 'is-invalid' : '' }}" name="product_id"
+                        id="product_id" required>
+                        @foreach ($products as $id => $entry)
+                            <option value="{{ $id }}" {{ old('product_id') == $id ? 'selected' : '' }}>
+                                {{ $entry }}</option>
+                        @endforeach
+                    </select>
+                    @if ($errors->has('product'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('product') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.sale.fields.product_helper') }}</span>
+                </div>
+                <div class="form-group">
+                    <label class="required" for="quantity">{{ trans('cruds.sale.fields.quantity') }}</label>
+                    <input class="form-control {{ $errors->has('quantity') ? 'is-invalid' : '' }}" type="number"
+                        name="quantity" id="quantity" value="{{ old('quantity', '') }}" step="1" required>
+                    @if ($errors->has('quantity'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('quantity') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.sale.fields.quantity_helper') }}</span>
+                </div>
+                <div class="form-group">
+                    <label class="required" for="sold_by_id">{{ trans('cruds.sale.fields.sold_by') }}</label>
+                    <select class="form-control select2 {{ $errors->has('sold_by') ? 'is-invalid' : '' }}"
+                        name="sold_by_id" id="sold_by_id" required>
+                        @foreach ($sold_bies as $id => $entry)
+                            <option value="{{ $id }}" {{ old('sold_by_id') == $id ? 'selected' : '' }}>
+                                {{ $entry }}</option>
+                        @endforeach
+                    </select>
+                    @if ($errors->has('sold_by'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('sold_by') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.sale.fields.sold_by_helper') }}</span>
+                </div>
+                <div class="form-group">
+                    <label class="required" for="total">{{ trans('cruds.sale.fields.total') }}</label>
+                    <input class="form-control {{ $errors->has('total') ? 'is-invalid' : '' }}" type="number"
+                        name="total" id="total" value="{{ old('total', '0') }}" step="0.01" required>
+                    @if ($errors->has('total'))
+                        <div class="invalid-feedback">
+                            {{ $errors->first('total') }}
+                        </div>
+                    @endif
+                    <span class="help-block">{{ trans('cruds.sale.fields.total_helper') }}</span>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-danger" type="submit">
+                        {{ trans('global.save') }}
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
-
-    <div class="card-body">
-        <form method="POST" action="{{ route("admin.sales.store") }}" enctype="multipart/form-data">
-            @csrf
-            <div class="form-group">
-                <label class="required" for="product_id">{{ trans('cruds.sale.fields.product') }}</label>
-                <select class="form-control select2 {{ $errors->has('product') ? 'is-invalid' : '' }}" name="product_id" id="product_id" required>
-                    @foreach($products as $id => $entry)
-                        <option value="{{ $id }}" {{ old('product_id') == $id ? 'selected' : '' }}>{{ $entry }}</option>
-                    @endforeach
-                </select>
-                @if($errors->has('product'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('product') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.sale.fields.product_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label class="required" for="quantity">{{ trans('cruds.sale.fields.quantity') }}</label>
-                <input class="form-control {{ $errors->has('quantity') ? 'is-invalid' : '' }}" type="number" name="quantity" id="quantity" value="{{ old('quantity', '') }}" step="1" required>
-                @if($errors->has('quantity'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('quantity') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.sale.fields.quantity_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label class="required" for="sold_by_id">{{ trans('cruds.sale.fields.sold_by') }}</label>
-                <select class="form-control select2 {{ $errors->has('sold_by') ? 'is-invalid' : '' }}" name="sold_by_id" id="sold_by_id" required>
-                    @foreach($sold_bies as $id => $entry)
-                        <option value="{{ $id }}" {{ old('sold_by_id') == $id ? 'selected' : '' }}>{{ $entry }}</option>
-                    @endforeach
-                </select>
-                @if($errors->has('sold_by'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('sold_by') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.sale.fields.sold_by_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <label class="required" for="total">{{ trans('cruds.sale.fields.total') }}</label>
-                <input class="form-control {{ $errors->has('total') ? 'is-invalid' : '' }}" type="number" name="total" id="total" value="{{ old('total', '0') }}" step="0.01" required>
-                @if($errors->has('total'))
-                    <div class="invalid-feedback">
-                        {{ $errors->first('total') }}
-                    </div>
-                @endif
-                <span class="help-block">{{ trans('cruds.sale.fields.total_helper') }}</span>
-            </div>
-            <div class="form-group">
-                <button class="btn btn-danger" type="submit">
-                    {{ trans('global.save') }}
-                </button>
-            </div>
-        </form>
-    </div>
-</div>
-
-
-
 @endsection
